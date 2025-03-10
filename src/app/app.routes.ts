2b28db4f3bfa810730e12a74ba102df73f841326
Max-Polster <git@max-polster.at>
2025-03-10 18:01:05 +0100

    Fisrt Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { FruitListComponent } from './fruitlist/fruitlist.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: FruitListComponent }];
